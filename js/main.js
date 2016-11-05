28ec1c6e8fb8d0acc16a4d04aae22267847aaeb2
Santiago Jaramillo <santiagojaramillo@Santiagos-MacBook-Pro.local>
2016-11-05 10:37:39 -0400

    my first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  var requestURL = "../data.json";
+  
+  $.ajax({
+    method: "GET",
+    url: requestURL,
+    dataType: "json",
+    success: mySuccessListener,
+    error: myErrorListener
+  });
+
+
+  
+});
+
+function mySuccessListener(data)
+{
+  alert('got it');
+}
+
+function myErrorListener(data)
+{
+  alert('error');
+}
+
 
